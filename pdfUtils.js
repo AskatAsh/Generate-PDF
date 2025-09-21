272dd7411947301f5beffe7cee296aa986480ff0
AskatAsh <abdulaskat@gmail.com>
2025-09-21 21:09:25 +0600

    called executablePath method

--- pdfUtils.js
+++ pdfUtils.js
@@ -5,7 +5,7 @@ export async function generatePDF(html) {
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
+    executablePath: await chromium.executablePath(),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   });
